b659341e69b181e130dd27aa897523dcd01de7d1
Jose Acosta <55077332+jchaidez23@users.noreply.github.com>
2022-10-05 02:41:28 -0700

    Created rough draft for home page
    Added all the required buttons and labels in order to have a working prototype of our home page

--- firebasetest/lib/screens/home_screen.dart
+++ firebasetest/lib/screens/home_screen.dart
@@ -16,21 +16,102 @@ class HomeScreen extends StatelessWidget {
         width: size.width,
         height: size.height,
         color: Colors.white,
-        child: const SingleChildScrollView(
+        child:  SingleChildScrollView(
           child: Padding(
-            padding:  EdgeInsets.only(
+            padding:  const EdgeInsets.only(
                 left: 16.0, right: 16.0, top: 50.0, bottom: 25.0),
-          child:  Align(
-            child:  Text(
-              'HOME',
-              style: TextStyle(
-                fontFamily: 'Cherry',
-                fontSize: 70,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+          child:  Column(
+            children: [
+               const Align(
+                  child:  Text(
+                    'Home',
+                    style: TextStyle(
+                      fontFamily: 'Cherry',
+                      fontSize: 70,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  )
               ),
-              textAlign: TextAlign.center,
-            )
+              const SizedBox(height: 20),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  size: 24.0,
+                ),
+                label: const Text('Create a Post'),
+                style: OutlinedButton.styleFrom(
+                  //Button size in order to take up the whole screen
+                  minimumSize: const Size.fromHeight(75),
+                  shape: const StadiumBorder(),
+                  //colors
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  //width of the border for our button
+                  side: const BorderSide(width: 5.0, color: Colors.black),
+                  //centers text
+                  alignment: Alignment.center,
+                  textStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),
+                ),
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.person,
+                  size: 24.0,
+                ),
+                label: const Text('Manage Accounts'),
+                style: OutlinedButton.styleFrom(
+                  //Button size in order to take up the whole screen
+                  minimumSize: const Size.fromHeight(75),
+                  shape: const StadiumBorder(),
+                  //colors
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  //width of the border for our button
+                  side: const BorderSide(width: 5.0, color: Colors.black),
+                  //centers text
+                  alignment: Alignment.center,
+                  textStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),
+                ),
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton.icon(
+                  onPressed: (){},
+                  icon: const Icon(
+                    Icons.settings,
+                    size: 24.0,
+                  ),
+                  label: const Text('App Settings'),
+                  style: OutlinedButton.styleFrom(
+                    //Button size in order to take up the whole screen
+                    minimumSize: const Size.fromHeight(75),
+                    shape: const StadiumBorder(),
+                    //colors
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    //width of the border for our button
+                    side: const BorderSide(width: 5.0, color: Colors.black),
+                    //centers text
+                    alignment: Alignment.center,
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0),
+                  ),
+              ),
+              const SizedBox(height: 60),
+              OutlinedButton(
+                  onPressed: () {},
+                  child: const Text(
+                      'Log Out',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                  ),
+              )
+            ],
           ),
 
           ),
