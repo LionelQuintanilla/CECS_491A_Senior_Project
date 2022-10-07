8b08fc6715dafe7cb0375de75f12ecb0085ffb2c
Jose Acosta <55077332+jchaidez23@users.noreply.github.com>
2022-10-07 02:57:09 -0700

    Added Text and backarrow to createpost

--- firebasetest/lib/screens/create_post.dart
+++ firebasetest/lib/screens/create_post.dart
@@ -28,10 +28,60 @@ class _CreatePost extends State<CreatePost>{
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 16.0, right: 16.0, top: 50.0, bottom: 25.0),
+            child: Form(
+              key: _key,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Image.asset('assets/images/backArrow.png',
+                          height: 40,
+                          width: 40)
+                  ),
+                  const Align(
+                    child: Text(
+                      'New',
+                      style: TextStyle(
+                        fontFamily: 'Cherry',
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Align(
+                    child: Text(
+                      'Post',
+                      style: TextStyle(
+                        fontFamily: 'Cherry',
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
