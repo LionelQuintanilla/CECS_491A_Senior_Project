1523bd4b69bd21511395b4e95a91d96bca3e3bef
LionelQuintanilla <Lionel.Quintanilla@student.csulb.edu>
2022-10-16 17:36:38 -0700

    Added verification screen styling

--- firebasetest/lib/screens/verification_screen.dart
+++ firebasetest/lib/screens/verification_screen.dart
@@ -4,7 +4,11 @@ import '/components/custom_button.dart';
 import '/core/authentication_service.dart';
 import '/screens/login.dart';
 import '/utils/loader.dart';
+import '/screens/customer_support.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '/core/authentication_service.dart';
+
 
 class VerifyScreen extends StatelessWidget {
   const VerifyScreen({Key? key}) : super(key: key);
@@ -12,46 +16,185 @@ class VerifyScreen extends StatelessWidget {
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          height: size.height,
+      body: Container(
           width: size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/blank-dp.png'),
-                  ),
+          height: size.height,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 16.0,right: 16.0, top: 50.0, bottom: 25.0),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Image.asset('assets/images/backArrow.png',
+                            height: 40,
+                            width: 40)
+                    ),
+                    const Align(
+                      child:
+                      Text(
+                        'Verify',
+                        style: TextStyle(
+                          fontFamily: 'Cherry',
+                          fontSize: 75,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const Align(
+                      child:
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontFamily: 'Cherry',
+                          fontSize: 70,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    const Align(
+                      child:
+                      Text(
+                        "Please verify your email before logging in. If you do not receive a verification email automatically, press the button below.",
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    Align(
+                      child:
+                      Text(
+                        'Email: ${AuthenticationService.auth.currentUser!.email}',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: SizedBox(
+                            width: 300,
+                            height: 75,
+                            child: OutlinedButton(
+                              onPressed: () async{
+                                //LoaderX.show(context);
+                                AuthenticationService.auth.currentUser!.sendEmailVerification();
+                                /*
+                                while (AuthenticationService.auth.currentUser!.emailVerified == false) {
+                                  AuthenticationService.auth.currentUser!.reload();
+                                }
+                                */
+                                //LoaderX.hide();
+                              },
+                              child: const Text('Resend Email'),
+                              style: OutlinedButton.styleFrom(
+                                minimumSize: const Size.fromHeight(75),
+                                shape: const StadiumBorder(),
+                                backgroundColor: Colors.white,
+                                foregroundColor: Colors.black,
+                                side: const BorderSide(width: 5.0, color: Colors.black),
+                                alignment: Alignment.center,
+                                textStyle: const TextStyle( fontWeight: FontWeight.bold, fontSize: 40.0),
+                              ),
+                            ),
+                          ),
+                        )
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: SizedBox(
+                            width: 300,
+                            height: 75,
+                            child: OutlinedButton(
+                              onPressed: () async {
+                                Navigator.pushNamedAndRemoveUntil(context, LoginScreen.id, (route) => false);
+                              },
+                              child: const Text('Log In'),
+                              style: OutlinedButton.styleFrom(
+                                minimumSize: const Size.fromHeight(75),
+                                shape: const StadiumBorder(),
+                                backgroundColor: Colors.white,
+                                foregroundColor: Colors.black,
+                                side: const BorderSide(width: 5.0, color: Colors.black),
+                                alignment: Alignment.center,
+                                textStyle: const TextStyle( fontWeight: FontWeight.bold, fontSize: 40.0),
+                              ),
+                            ),
+                          ),
+                        )
+                    ),
+                    const SizedBox(height:20),
+                    GestureDetector(
+                        onTap: () => Navigator.pushNamed(context, CustomerSupport.id),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: RichText(
+                            textScaleFactor: .9,
+                            text: const TextSpan(
+                                text: "Customer Support",
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                )
+                            ),
+                          ),
+                        )
+                    ),
+                    const SizedBox(height:20),
+                    GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: RichText(
+                            textScaleFactor: .9,
+                            text: const TextSpan(
+                                text: "Cancel",
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                )
+                            ),
+                          ),
+                        )
+                    )
+                  ],
                 ),
               ),
-              /*
-              const SizedBox(height: 10),
-              Text(
-                'Hi, ${AuthenticationService.auth.currentUser!.displayName}.',
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            ),
+          )
+      ),
+    );
+  }
+}
 
-               */
-              const SizedBox(height: 10),
-              const Text(
-                'Please verify your email before logging in. If you did not automatically receive an email, press the button below',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 10),
+/*
+const SizedBox(height: 10),
               Text(
                 'Email: ${AuthenticationService.auth.currentUser!.email}',
                 style: const TextStyle(
@@ -59,11 +202,27 @@ class VerifyScreen extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-
-              SizedBox(
+ */
+/*
+SizedBox(
                 width: 200,
                 child: CustomButton(
-                  label: 'SEND EMAIL',
+                  label: 'Log In',
+                  color: Colors.black,
+                  onPressed: () async {
+                    Navigator.pushNamedAndRemoveUntil(context, LoginScreen.id, (route) => false);
+                  },
+                  size: size,
+                  textColor: Colors.white,
+                  borderSide: BorderSide.none,
+                ),
+              )
+ */
+/*
+SizedBox(
+                width: 200,
+                child: CustomButton(
+                  label: 'Send Verification Email',
                   color: Colors.black,
                   onPressed: () async {
                     //LoaderX.show(context);
@@ -80,23 +239,4 @@ class VerifyScreen extends StatelessWidget {
                   borderSide: BorderSide.none,
                 ),
               ),
-              SizedBox(
-                width: 200,
-                child: CustomButton(
-                  label: 'LOGIN',
-                  color: Colors.black,
-                  onPressed: () async {
-                    Navigator.pushNamedAndRemoveUntil(context, LoginScreen.id, (route) => false);
-                  },
-                  size: size,
-                  textColor: Colors.white,
-                  borderSide: BorderSide.none,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+ */
