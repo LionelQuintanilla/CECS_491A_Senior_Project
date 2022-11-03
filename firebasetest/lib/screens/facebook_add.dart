e5853100c6eca6e0f065ffce94ea9c07b6e66f41
Destin <destin4235@gmail.com>
2022-11-02 18:39:25 -0700

    Added functionality for the add facebook account page to launch the facebook app from WUPHF app

--- firebasetest/lib/screens/facebook_add.dart
+++ firebasetest/lib/screens/facebook_add.dart
@@ -13,6 +13,9 @@ import 'package:flutter/material.dart';
 import 'package:twitter_api_v2/twitter_api_v2.dart';
 import 'package:twitter_oauth2_pkce/twitter_oauth2_pkce.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
 import 'dart:async';
 import 'dart:io';
 
@@ -103,7 +106,14 @@ class _FacebookAdd extends State<FacebookAdd>{
                     ),
                     const SizedBox(height: 35),
                     OutlinedButton.icon(
-                      onPressed: () {},
+                      onPressed: () async {
+                        const String facebookUrl = 'https://facebook.com/';
+                        if (await canLaunchUrlString(facebookUrl)) {
+                          await launchUrlString(facebookUrl);
+                        } else {
+                          throw 'Could not launch $facebookUrl';
+                        }
+                      },
                       icon: const Icon(
                         Icons.people,
                         size: 50.0,
