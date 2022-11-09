6a45abe3e715ca929b5db6e1cd1d222e3031791e
Jose Acosta <55077332+jchaidez23@users.noreply.github.com>
2022-11-09 14:36:19 -0800

    create_post added image display
    -Changed Icon to avatar to display the image that is selected on the screen
    -Going to need to create a custom box to reflect a box shape for pictures.

--- firebasetest/lib/screens/create_post.dart
+++ firebasetest/lib/screens/create_post.dart
@@ -36,11 +36,9 @@ class _CreatePost extends State<CreatePost>{
       File? img = File(image.path);
       setState(() {
         _image = img;
-        Navigator.of(context).pop();
       });
     } on PlatformException catch (e) {
       print(e);
-      Navigator.of(context).pop();
     }
   }
   final _key = GlobalKey<FormState>();
@@ -103,10 +101,20 @@ class _CreatePost extends State<CreatePost>{
                   Align(
                         child: ConstrainedBox(
                             constraints: const BoxConstraints(minHeight: 200),
-                            child: const Icon(
-                              Icons.add,
-                              size: 300,
-                              color: Colors.red,
+                            child: (
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Container(
+                                  height: 300,
+                                  width: 300,
+                                  child: CircleAvatar(
+                                    backgroundImage: _image == null
+                                    ? null
+                                    : FileImage(_image!,
+                                    )
+                                  )
+                                ),
+                              )
                             ),
                         ),
                             alignment: Alignment.center
